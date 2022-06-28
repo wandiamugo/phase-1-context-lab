454353da88d83cfd787f6a97510d2de5e4ae7ed4
Wandia E Mugo <wwandia173@gmail.com>
2022-06-28 19:25:28 +0300

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,75 @@
 /* Your Code Here */
+let createEmployeeRecord = function(employeeArray){
+    return {
+      firstName: employeeArray[0],
+      familyName: employeeArray[1],
+      title: employeeArray[2],
+      payPerHour: employeeArray[3],
+      timeInEvents: [],
+      timeOutEvents: []
+    }
+  }
+
+  function createEmployeeRecords(rows){
+   return rows.map(createEmployeeRecord)
+  }
+
+const getHour = function(dateTime){
+    return parseInt(dateTime.match(/\d{4}$/)[0])
+   }
+ //function createTimeInEvent(array, dateStamp){
+  //   let employees = createEmployeeRecord[array]
+   //  employees.timeInEvents.push(dateStamp)
+     
+ //}
+ const getDate = function(dateTime) {
+     return dateTime.match(/\d{4}-\d{2}-\d{2}/)[0]
+ }
+
+ function createTimeInEvent(timeIn){
+      this.timeInEvents.push({
+        type: "TimeIn",
+        date: getDate(timeIn),
+        hour:getHour(timeIn)
+    })
+    return this;
+}
+
+function createTimeOutEvent(timeOut){
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        date: getDate(timeOut),
+        hour:getHour(timeOut)
+    })
+    return this;
+}
+
+function hoursWorkedOnDate(dateGiven) {
+    let timeIn = this.timeInEvents.find(event =>
+        event.date == dateGiven)
+    let timeOut = this.timeOutEvents.find(event =>
+        event.date == dateGiven)
+    let totalTime = (timeOut.hour - timeIn.hour) / 100
+    return totalTime;
+}
+
+function wagesEarnedOnDate( dateGiven) {
+    let hours = hoursWorkedOnDate.call(this , dateGiven)
+    return this.payPerHour * hours;
+}
+
+function findEmployeeByFirstName(srcArray, firstName){
+    return srcArray.find(employee => employee.firstName === firstName)
+}
+
+function calculatePayroll(employeeRecord){
+    return employeeRecord.reduce((total, employee) => {
+        return total + allWagesFor.call(employee)
+    }, 0)
+}
+
+
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
